be558369b592109a54498cf032db20c1cbfa5087
germanblanco <german.blanco@gmail.com>
2016-01-13 11:24:48 +0100

    Cleanup.

--- CustomEncoder.java
+++ CustomEncoder.java
@@ -1,21 +1,11 @@
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import net.logstash.logback.marker.LogstashMarker;
+import net.logstash.logback.encoder.LogstashEncoder;
 import static net.logstash.logback.marker.Markers.*;
 import java.io.IOException;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import ch.qos.logback.core.CoreConstants;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonGenerator.Feature;
 
-public class CustomEncoder extends net.logstash.logback.encoder.LogstashEncoder {
-
-    private static final ObjectMapper MAPPER = new ObjectMapper().configure(Feature.ESCAPE_NON_ASCII, true);
-
-    private boolean immediateFlush = false;
-
-    public CustomEncoder() {
-    }
+public class CustomEncoder extends LogstashEncoder {
 
     @Override
     public void doEncode(ILoggingEvent event) throws IOException {
